567cb96190112fdb7e7404ac70581a99e19bbb13
cursoversionamento <cursoversionamento@gmail.com>
2022-05-06 15:49:39 -0300

    Camada de modelo

---
+++ professor-allocation-rafael/src/test/java/com/project/professor/allocation/rafael/ProfessorAllocationRafaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.rafael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationRafaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
